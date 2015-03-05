39a79764fd5b7ba6242c5d6d5e9104ff54388cb7
LegeayJc <legeayjc@yahoo.fr>
2015-03-05 17:36:27 +0100

    Right controller update
    A faire Mettre le compteur en place pour radio- button candidats
    acceptés

--- workspace/MockUp/src/controller/DirectorRightController.java
+++ workspace/MockUp/src/controller/DirectorRightController.java
@@ -3,14 +3,19 @@ package controller;
 import java.util.Iterator;
 
 import model.LectureUVFichier;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -19,7 +24,6 @@ import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-
 
 /**
  * 
@@ -32,11 +36,9 @@ public class DirectorRightController {
 	private String sessionToken;
 	private DirectorController director;
 	
-
 	@FXML
     private GridPane gridCandidats;
 	
-
 	public void loadCandidats(ObservableList<String> token) {
 		loadGrid(token);
     }
@@ -46,27 +48,35 @@ public class DirectorRightController {
 		director = directorController;
 	}
 		
-	
 	private void loadGrid(ObservableList<String> names) {
 
 		gridCandidats.getChildren().clear(); // clear gridPane
 		
 		// setup column titles
+		//Label counter = new Label("Candidats acceptés");
+		Label candi = new Label(" Candidatures acceptées   ");
+		TextField counter= new TextField("0");
+		counter.setPrefWidth(40);
 		Text txtNameTitle = new Text("Noms des Candidats");
 		Text txtAccepteTitle = new Text("Accepté");
 		Text txtRefuseTitle = new Text("Refusé");
 		Text txtAttenteTitle = new Text("Liste d'Attente");
+		
 		HBox hbTitle = new HBox(txtAccepteTitle, txtRefuseTitle, txtAttenteTitle);
-		hbTitle.setSpacing(100);
+		HBox boxCounter = new HBox(candi,counter);
+		boxCounter.setPadding(new Insets(10, 10, 10, 10));
+		hbTitle.setSpacing(65);
 		hbTitle.setPadding(new Insets(10, 10, 10, 10));
 		//txtNameTitle.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		
 		gridCandidats.add(txtNameTitle,0,0); // (txtNameTitle, column, row)
 		gridCandidats.add(hbTitle,1,0);
+		gridCandidats.add(boxCounter,0,1000);
 		
 		Iterator<String> iter = names.iterator();
-		int i=10;
+		int i=1;
+		
 		while (iter.hasNext()){
-			
 			// setup of RadioButtons
 			RadioButton rdoAttente = new RadioButton(); 
 			RadioButton rdoRefuse = new RadioButton(); 
@@ -80,9 +90,10 @@ public class DirectorRightController {
 			rdoAttente.setVisible(true);
 			rdoRefuse.setVisible(true);
 			rdoAccepte.setVisible(true);
+			radioGroup.selectToggle(rdoAttente);
 			
 			// putting RadioButtons into HBox for easy horizontal distribution
-			HBox hb = new HBox(rdoAttente, rdoRefuse, rdoAccepte);
+			HBox hb = new HBox(rdoAccepte, rdoRefuse, rdoAttente);
 			hb.setSpacing(100);
 			hb.setPadding(new Insets(10, 10, 10, 10));
 			
@@ -92,6 +103,26 @@ public class DirectorRightController {
 			gridCandidats.add(txtName, 0, i);
 			gridCandidats.add(hb,1,i);
 			i++;
+			
+			// try to increase counter everytime the radio button is selected
+						
+			
+			radioGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+							        { @Override
+							 			
+							            public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle, Toggle new_toggle)
+							            {
+							            					        	
+							             int count=0;
+							                if (new_toggle == null)
+							                {
+							                }
+							                else
+							                { 
+							                	count ++;	counter.setText(""+count);	                  		                    
+							                }
+							            } 
+							        });
 		}
 		
 	}
