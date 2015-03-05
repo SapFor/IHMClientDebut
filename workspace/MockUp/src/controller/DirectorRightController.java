a6965f5804a727618d32c54137bb3d7318144acd
May Workman <may.workman@agapefrance.org>
2015-03-05 15:07:06 +0100

    gridPane with names and radio buttons functioning

--- workspace/MockUp/src/controller/DirectorRightController.java
+++ workspace/MockUp/src/controller/DirectorRightController.java
@@ -16,6 +16,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 
 /**
@@ -26,85 +29,50 @@ import javafx.scene.layout.RowConstraints;
 
 public class DirectorRightController {
 
-	//private DirectorController director;
 	private String sessionToken;
 	private DirectorController director;
 	
-/*	@FXML
-    private Label nom;
-	
-	@FXML
-    private Label prenom;
-	
-	@FXML
-    private ListView<String> listNom;
-	
-	@FXML
-    private ListView<Pane> listRadio;*/
-	
+
 	@FXML
     private GridPane gridCandidats;
 	
-/*	@FXML
-    private ToggleGroup radioGroup;
-	
-	@FXML
-    private RadioButton rdoAttente;
-	
-	@FXML
-    private RadioButton rdoRefuse;
-	
-	@FXML
-    private RadioButton rdoAccepte;
-*/	
-		
-				
-/*	public void init(DirectorController directorController) {
-		director = directorController;
-	}
-	
-	public void getSessionToken(String token){
-		sessionToken = token;
-	}*/
 
 	public void loadCandidats(ObservableList<String> token) {
-		//gridCandidats.setPadding(new Insets(100, 100, 100, 100));
-		//gridCandidats.setMargin(node, new Insets(50, 10, 5, 10));
 		loadGrid(token);
-		
-		
-		
-	//	gridCandidats.addrow();
-    //	listNom.setItems(token);
-    	
-    	
-    	
- //   	ObservableList<Pane> myRadioBtnList = FXCollections.observableArrayList (paneRadio);
- //   	myRadioBtnList.add(paneRadio);
- //   	paneRadio.setVisible(true);
-    	
-  // 		makeRadioButtonsPane(token.size());
-  // 			myRadioBtnList.add(paneRadio);
-  //  		paneRadio.setVisible(true);
-
-    	//listRadio.setItems(myRadioBtnList);
-
     }
 
-    
-    
+
 	public void init(DirectorController directorController) {
 		director = directorController;
 	}
 		
 	
 	private void loadGrid(ObservableList<String> names) {
+
+		gridCandidats.getChildren().clear(); // clear gridPane
+		
+		// setup column titles
+		Text txtNameTitle = new Text("Noms des Candidats");
+		Text txtAccepteTitle = new Text("Accepté");
+		Text txtRefuseTitle = new Text("Refusé");
+		Text txtAttenteTitle = new Text("Liste d'Attente");
+		HBox hbTitle = new HBox(txtAccepteTitle, txtRefuseTitle, txtAttenteTitle);
+		hbTitle.setSpacing(100);
+		hbTitle.setPadding(new Insets(10, 10, 10, 10));
+		//txtNameTitle.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		gridCandidats.add(txtNameTitle,0,0); // (txtNameTitle, column, row)
+		gridCandidats.add(hbTitle,1,0);
+		
 		Iterator<String> iter = names.iterator();
-		int i=1;
+		int i=10;
 		while (iter.hasNext()){
+			
+			// setup of RadioButtons
 			RadioButton rdoAttente = new RadioButton(); 
 			RadioButton rdoRefuse = new RadioButton(); 
 			RadioButton rdoAccepte = new RadioButton(); 
+			
+			// only one RadioButton belonging to toggleGroup can be choosen at a given moment 
 			ToggleGroup radioGroup = new ToggleGroup();
 			rdoAttente.setToggleGroup(radioGroup);
 			rdoRefuse.setToggleGroup(radioGroup);
@@ -113,16 +81,19 @@ public class DirectorRightController {
 			rdoRefuse.setVisible(true);
 			rdoAccepte.setVisible(true);
 			
+			// putting RadioButtons into HBox for easy horizontal distribution
 			HBox hb = new HBox(rdoAttente, rdoRefuse, rdoAccepte);
 			hb.setSpacing(100);
 			hb.setPadding(new Insets(10, 10, 10, 10));
-			Label lblName = new Label();
-			lblName.setText(iter.next());
-			gridCandidats.add(lblName, 0, i);
-			gridCandidats.add(hb,1,i);
 			
+			Text txtName = new Text();
+			txtName.setText(iter.next());
+			// adding names and RadioButtons to gridPane
+			gridCandidats.add(txtName, 0, i);
+			gridCandidats.add(hb,1,i);
 			i++;
 		}
+		
 	}
 }
 
