f7bb580f56be715bb2abfc6a5f2a11c4d1b60b2f
lmalg <leomalgrange@gmail.com>
2015-03-05 21:55:09 +0100

    New

--- workspace/MockUp/src/controller/FormationController.java
+++ workspace/MockUp/src/controller/FormationController.java
@@ -1,14 +1,20 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,10 +23,33 @@ import javafx.scene.control.Button;
 
 public class FormationController implements Initializable{
 
-	@FXML private SplitMenuButton btnFormation;
-
+	@FXML
+	private TableView<String> UVTable;
+	
+	@FXML
+    private TableColumn<String, String> UVColumn;
+	
+	@FXML
+	private SplitMenuButton btnFormation;
+	
+    //private ObservableList<String> ListeUV = FXCollections.observableArrayList();
+	
+	private void chargementUV (ObservableList<String> ListeUV){
+		
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		ArrayList<String> ListeUVList = new  ArrayList<String>();
+		ListeUVList.add("INC1");
+		ListeUVList.add("INC2");
+		ListeUVList.add("FORM1");
+		ListeUVList.add("FORM2");
+		
+		ObservableList<String> ListeUV = FXCollections.observableArrayList(ListeUVList);
+		
+        UVColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
 
 	}
 
