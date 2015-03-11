e0689fe5fbdf5f7b34542e0cb588fda531755386
TanguyDavin <tanguydavin@Tanguy.local>
2015-03-11 10:43:19 +0100

    UV

--- workspace/MockUp/src/controller/FormationController.java
+++ workspace/MockUp/src/controller/FormationController.java
@@ -10,22 +10,23 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.HBox;
 
 public class FormationController implements Initializable{
 
 	//Liste des UV accessibles au candidat
 	@FXML
 	private ListView<String> UVList;
+
 	
 	//Boutons radio pour trier selon apprenant/formateur et groupe pour n'en avoir qu'un seuld e sélectionné
 	@FXML
 	private RadioButton boutonApprenant;
+
 	@FXML
 	private RadioButton boutonFormateur;
 	@FXML
@@ -33,12 +34,21 @@ public class FormationController implements Initializable{
 	
 	@FXML
 	private TextArea UVDesc;
-	
+
 	@FXML
 	private ListView<String> SessionList;
-	
+
 	@FXML
 	private TextArea InfoSession;
+
+
+	@FXML
+	private Button candidaterBt;
+
+	@FXML
+	private Button retirerBt;
+
+
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -49,34 +59,66 @@ public class FormationController implements Initializable{
 		ListeUVList.add("DEV1");
 		ListeUVList.add("OC1");
 		
+
 		ObservableList<String> ListeUV = FXCollections.observableArrayList(ListeUVList);
-		
+
 		UVList.setItems(ListeUV);
 	}
 
-	@FXML
-    void clicUV(Event event) {
-    	
-    	//String items =UVList.getSelectionModel().getSelectedItem();
-    	//L'affichage de la descritpion de l'UV
-    	String description= new String("la description de l'UV");
-    	UVDesc.setText(description);
-    	
-    	//pour ne pas modifier le text area dans le programme
-    	UVDesc.setEditable(false);
-    	
-    	//l'affichage des sessions 
-    	ArrayList<String> SessionUVList = new  ArrayList<String>();
-    	SessionUVList.add("Rennes");
-    	SessionUVList.add("St Malo");
-    	SessionUVList.add("Redon");
-		
-		ObservableList<String> SessionUV = FXCollections.observableArrayList(SessionUVList);
-		
-		SessionList.setItems(SessionUV);
-    }
+
+		//UVColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+
 	
+
+	@FXML private void btnFormation1Clicked(ActionEvent event){
+		System.out.println("Button Formation Clicked");
+	}
+
+
+
 	@FXML
+	void clicUV(Event event) {
+
+		//String items =UVList.getSelectionModel().getSelectedItem();
+		//L'affichage de la descritpion de l'UV
+		String description= new String("la description de l'UV");
+		UVDesc.setText(description);
+
+		//pour ne pas modifier le text area dans le programme
+		UVDesc.setEditable(false);
+
+
+
+
+
+		String UVclic = UVList.getSelectionModel().getSelectedItem();
+
+		if (UVclic=="INC1"){
+			//l'affichage des sessions 
+			ArrayList<String> SessionUVList = new  ArrayList<String>();
+			SessionUVList.add("Rennes");
+			SessionUVList.add("St Malo");
+			SessionUVList.add("Redon");
+			ObservableList<String> SessionUV = FXCollections.observableArrayList(SessionUVList);
+
+			SessionList.setItems(SessionUV);}
+		else {
+
+			ArrayList<String> SessionUVList = new  ArrayList<String>();
+			SessionUVList.add("Nantes");
+			SessionUVList.add("Laval");
+			SessionUVList.add("Reims");
+			ObservableList<String> SessionUV = FXCollections.observableArrayList(SessionUVList);
+
+			SessionList.setItems(SessionUV);
+
+		}
+
+	}
+
+
+	@FXML
+	
     void clicSession(Event event) {
     	
     	//String items =UVList.getSelectionModel().getSelectedItem();
@@ -87,6 +129,8 @@ public class FormationController implements Initializable{
     	
     	//pour ne pas modifier le text area dans le programme
     	InfoSession.setEditable(false);
+		candidaterBt.setVisible(true);
+
     }
 
 	/**
@@ -121,4 +165,21 @@ public class FormationController implements Initializable{
 		UVList.setItems(ListeUV);
 	}
 	
+
+	@FXML
+	void clicCandidater(Event event) {
+		candidaterBt.setVisible(false);
+		retirerBt.setVisible(true);
+
+	}
+	
+	
+	@FXML
+	void clicRetirer(Event event) {
+		candidaterBt.setVisible(true);
+		retirerBt.setVisible(false);
+
+	}
+
+
 }
